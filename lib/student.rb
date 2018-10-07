1bd918353264058c15cdf12e23da79f6b9d099ae
Heidi Hlavinka <hchlav@gmail.com>
2018-10-07 23:19:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,7 +78,8 @@ class Student
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql).map binding.pry
+    DB[:conn].execute(sql).map 
+    binding.pry
     do |row|
       self.new_from_db(row)
     end

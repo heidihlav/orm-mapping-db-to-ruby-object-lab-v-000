bd50724493f6a93f7519124e4b1347407400495a
Heidi Hlavinka <hchlav@gmail.com>
2018-10-07 23:38:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -84,6 +84,16 @@ class Student
   end
 
   def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT COUNT(*)
+      FROM students
+      WHERE grade = 9
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
 
   end
 

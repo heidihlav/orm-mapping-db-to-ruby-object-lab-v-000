e1c02dabc888a8656e82fa535535261262876638
Heidi Hlavinka <hchlav@gmail.com>
2018-10-07 22:09:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,8 +11,8 @@ class Student
       new_student = self.new
       new_student.id = row[0]
       new_student.name = row[1]
-      new_student.length = row[2]
-      new_student 
+      new_student.grade = row[2]
+      new_student
        # create a new Student object given a row from the database
   end
 

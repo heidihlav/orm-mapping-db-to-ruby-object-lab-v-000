7e74ca31e136e2f69c82a0758aab4edd8aaa656d
Heidi Hlavinka <hchlav@gmail.com>
2018-10-08 00:06:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -110,10 +110,9 @@ class Student
 
   def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT COUNT(*)
+      SELECT *
       FROM students
       WHERE grade = 10
-      LIMIT 1
     SQL
 
     DB[:conn].execute(sql).map do |row|
